125228896c6458a84a690b4749578788b0f4423a
suresh <sureshkante.972@gmail.com>
2024-10-24 08:06:05 +0530

    first programme

--- src/main/scala/TES3.scala
+++
@@ -1,8 +0,0 @@
-object TEST1 {
-  def main(args:Array[String]):Unit=
-  {
-    println("hello world")
-
-  }
-
-}
--- src/main/scala/test3.scala
+++ src/main/scala/test3.scala
@@ -1,3 +1,8 @@
 object test3 {
-
+def main(args:Array[String]):Unit=
+  {
+    println("hello World")
+    var a =Array(1,2,3,4)
+    println(a(1))
+  }
 }
